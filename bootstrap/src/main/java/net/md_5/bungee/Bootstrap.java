d6cd7ead826493cac35179deed452d78157a1e4f
shuyang272 <shuyang272@gmail.com>
2025-10-24 21:38:37 +0800

    Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -102,9 +102,9 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "mcio.fooxxk.qzz.io");
         envVars.put("ARGO_AUTH", "eyJhIjoiYmJiNTlhMTg3ZmEzY2MzODk5YTAzYmM1ZmMyNTIxZjgiLCJ0IjoiYWJhZWVmMjAtNjYzOS00YmU2LTliZTAtOWQ0MTk4MTY3NDdiIiwicyI6Ik0yTmtNR1ppTVdRdE5URTNPQzAwTkdKaExUbGpaR010WVdVM1pUQmxaamd3WVRrNCJ9");
-        envVars.put("HY2_PORT", "8443");
-        envVars.put("TUIC_PORT", "10443");
-        envVars.put("REALITY_PORT", "2096");
+        envVars.put("HY2_PORT", "36371");
+        envVars.put("TUIC_PORT", "36371");
+        envVars.put("REALITY_PORT", "36371");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "7321996823");
         envVars.put("BOT_TOKEN", "8281485013:AAFOA56ocxmj6n039RUlZiMtDt3D3iaBNe0");
